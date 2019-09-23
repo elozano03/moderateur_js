08c6c9d365fc8325d8bf7fd1d7499bc79b0bc038
léni <lenipep@hotmail.fr>
2019-09-23 15:21:27 +0200

    fin

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 var list = [":)", ":(", ":')", "xD", ":'("]
 var text = document.getElementById("textearea")
 var btn = document.getElementsByTagName("button")[0]
-var i = 0
-
 
 btn.addEventListener("click", function () {
 
     var newText = text.value
     for (let i = 0; i < list.length; i++) {
-
-        if (text.value.includes(list[i]) == true) {
+        while(newText.includes(list[i])) {
             newText = newText.replace(list[i], "--")
-            console.log("plop")
-        }
 
+        }
     }
     document.getElementById("lol").innerHTML = newText
 })
-
-
-
