9ef4ab97cb094b2e761411408ae508936da780ca
Lozano Elian <Elian.lozano@outlook.fr>
2019-10-17 14:11:06 +0200

    Jquery version

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var list = [":)", ":(", ":')", "xD", ":'("]
-var text = document.getElementById("textearea")
-var btn = document.getElementsByTagName("button")[0]
+// var list = [":)", ":(", ":')", "xD", ":'("]
+// var text = document.getElementById("textearea")
+// var btn = document.getElementsByTagName("button")[0]
 
-btn.addEventListener("click", function () {
+// btn.addEventListener("click", function () {
 
-    var newText = text.value
-    for (let i = 0; i < list.length; i++) {
-        while(newText.includes(list[i])) {
-            newText = newText.replace(list[i], "--")
+//     var newText = text.value
+//     for (let i = 0; i < list.length; i++) {
+//         while (newText.includes(list[i])) {
+//             newText = newText.replace(list[i], "--")
 
+//         }
+//     }
+//     document.getElementById("div").innerHTML = newText
+// })
+
+$(document).ready(function () {
+    var l = [":)", ":(", ":')", "xD", ":'("]
+
+    $("button").click(function () {
+        var newText = $("textarea").html()
+        for (let i = 0; i < l.length; i++) {
+            while (newText.includes(l[i])) {
+                newText = newText.replace(l[i], "--")
+
+            }
         }
-    }
-    document.getElementById("lol").innerHTML = newText
-})
+    $("#div").html(newText)
+    })
+});
